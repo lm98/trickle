a064704358539934cd8f73495b78cacd58a4b2a4
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-04-05 16:32:33 +0200

    added keyboard event handling

--- examples/game.rs
+++ examples/game.rs
@@ -1,12 +1,15 @@
+use trickle::*;
+
 extern "C" {
     pub fn log_number(number: usize);
 }
 
 fn main() {
-    let mut blue_amount = 0.0;
-
-    trickle::set_event_handler(move || {
-        blue_amount += 0.1;
-        trickle::clear_screen_to_color(0.0, 0.0, blue_amount, 1.0);
+    set_event_handler(move |key| match key {
+        Key::Left => clear_screen_to_color(1.0, 0.0, 0.0, 1.0),
+        Key::Right => clear_screen_to_color(0.0, 1.0, 0.0, 1.0),
+        Key::Up => clear_screen_to_color(0.0, 0.0, 1.0, 1.0),
+        Key::Down => clear_screen_to_color(0.0, 1.0, 1.0, 1.0),
+        Key::Space => clear_screen_to_color(1.0, 1.0, 0.0, 1.0),
     });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,34 @@ pub fn clear_screen_to_color(red: f32, green: f32, blue: f32, alpha: f32) {
     }
 }
 
-#[no_mangle]
-pub extern "C" fn key_pressed() {
-    clear_screen_to_color(0.0, 1.0, 0.0, 1.0);
+pub enum Key {
+    Left,
+    Right,
+    Up,
+    Down,
+    Space
 }
+
+#[no_mangle]
+pub extern "C" fn key_pressed(value: usize) {
+    let key = match value {
+        1 => Key::Left,
+        2 => Key::Right,
+        3 => Key::Up,
+        4 => Key::Down,
+        5 => Key::Space,
+        _ => return,
+    };
+
+    EVENT_HANDLER.with(|event_handler| (event_handler.borrow_mut())(key))
+}
+
 
 thread_local! {
-    pub static EVENT_HANDLER: std::cell::RefCell<Box<dyn FnMut()>> 
-        = std::cell::RefCell::new(Box::new(||{}));
+    pub static EVENT_HANDLER: std::cell::RefCell<Box<dyn FnMut(Key)>> = std::cell::RefCell::new(Box::new(|_|{}));
 }
 
-pub fn set_event_handler(function: impl FnMut() + 'static) {
+pub fn set_event_handler(function: impl FnMut(Key) + 'static) {
     EVENT_HANDLER.with(|event_handler| {
         *event_handler.borrow_mut() = Box::new(function);
     });
